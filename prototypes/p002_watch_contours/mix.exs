5a7db92b97f91a4f1a2bd3db3604c5fbe2b4efe0
Brian Marick <marick@exampler.com>
2024-01-20 16:56:01 -0600

    Add flow-assertions

--- prototypes/p002_watch_contours/mix.exs
+++ prototypes/p002_watch_contours/mix.exs
@@ -22,7 +22,9 @@ defmodule P001ParagraphSplitter.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:common, path: "../common"}
+      {:common, path: "../common"},
+      {:private, "> 0.0.0"},
+      {:flow_assertions, "~> 0.6", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
