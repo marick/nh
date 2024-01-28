bbd6952424c0201677b98aaac35f3c08554211ab
Brian Marick <marick@exampler.com>
2024-01-28 15:35:59 -0600

    cleanup

--- prototypes/p002_watch_contours/lib/paragraph_focus/motor/move_fragment.ex
+++ prototypes/p002_watch_contours/lib/paragraph_focus/motor/move_fragment.ex
@@ -14,49 +14,91 @@ defmodule AppAnimal.ParagraphFocus.Motor.MoveFragment do
     [@summary, WithoutReply] # go keep from warning about unused aliases.
   end
 
-  private do 
+  private do
 
-    def grab_fragment(paragraph, at_roughly: range) do
-      with(
-        {:ok, first_text..last_text} <- accounting_for_edits(paragraph.text, range),
-        :safely_distant <- cursor_relationship(paragraph, first_text..last_text)
-      ) do 
-        {:ok, split_text(paragraph.text, at_exactly: {first_text-1, last_text+1})}
-      else
-        _ -> :error
-      end
+    def new_end(_text, _original_start.._original_end) do
     end
+
+    # Note: do_not_exceed is actually off by one, but that's irrelevant
+    # given the need for two newlines past the search bounds.
+    def allowed_range(original_start..original_end = range, do_not_exceed) do
+      distance = div(Range.size(range), 2) + 2 # we need enough to recognize the gap
+      left = max(0, original_start - distance)
+      right = min(original_end + distance, do_not_exceed)
+      left..right
+    end
+
     
-    def cursor_relationship(paragraph, lower_fragment_bound..upper_fragment_bound) do
-      if paragraph.cursor < lower_fragment_bound - 1
-         || paragraph.cursor > upper_fragment_bound + 2 do
-        :safely_distant
-      else
-        :too_close
+
+    def surrounding_gaps(text, bounds) do
+      substring = String.slice(text, bounds)
+      [[first_gap], [second_gap]] = (Regex.scan(~r/\n\n+/, substring, return: :index))
+
+      tuple_to_range = fn {start, length} -> start..(start+length-1) end
+      translate = fn in_substring_coordinates ->
+        translation = Enum.at(bounds, 0)
+        Range.shift(in_substring_coordinates, translation)
       end
+
+      {:ok,
+       tuple_to_range.(first_gap) |> translate.(),
+       tuple_to_range.(second_gap) |> translate.()
+      }
     end
 
-    @max_tries 4   # tries includes the initial try with no shift.
-    def accounting_for_edits(paragraph, range) do
-      perhaps_shift(paragraph, range, @max_tries)
+    def grip_fragment(%{text: text}, at_roughly: original_range) do
+      bounds = allowed_range(original_range, String.length(text))
+
+      {:ok, _..end_of_gap1, start_of_gap_2.._} = surrounding_gaps(text, bounds)
+
+      fragment_range = (end_of_gap1+1)..(start_of_gap_2-1)
+      {:ok, fragment_range}
     end
 
-    def perhaps_shift(_paragraph, _range, 0), do: :error
+    # def fragment_range_within_bounds(text, bounds) do
+    # end
+
+    # def grab_fragment(paragraph, at_roughly: range) do
+    #   with(
+    #     {:ok, first_text..last_text} <- accounting_for_edits(paragraph.text, range),
+    #     :safely_distant <- cursor_relationship(paragraph, first_text..last_text)
+    #   ) do 
+    #     {:ok, split_text(paragraph.text, at_exactly: {first_text-1, last_text+1})}
+    #   else
+    #     _ -> :error
+    #   end
+    # end
     
-    def perhaps_shift(paragraph, first..last, remaining_shifts) do
-      include_buffer = (first-3)..(last+1)
-      with_buffer = String.slice(paragraph, include_buffer)
-      if Regex.match?(~r/\n\n[^\n]+\n\n/, with_buffer) do
-        {:ok, first..last}
-      else
-        perhaps_shift(paragraph, (first+1)..(last+1), remaining_shifts - 1)
-      end
-    end
+    # def cursor_relationship(paragraph, lower_fragment_bound..upper_fragment_bound) do
+    #   if paragraph.cursor < lower_fragment_bound - 1
+    #      || paragraph.cursor > upper_fragment_bound + 2 do
+    #     :safely_distant
+    #   else
+    #     :too_close
+    #   end
+    # end
 
-    def split_text(text, at_exactly: {first, last}) do
-      {prefix, remainder} = String.split_at(text, first-1)
-      {fragment, suffix} = String.split_at(remainder, last-first+1)
-      [prefix, fragment, suffix]
-    end
+    # @max_tries 4   # tries includes the initial try with no shift.
+    # def accounting_for_edits(paragraph, range) do
+    #   perhaps_shift(paragraph, range, @max_tries)
+    # end
+
+    # def perhaps_shift(_paragraph, _range, 0), do: :error
+    
+    # def perhaps_shift(paragraph, first..last, remaining_shifts) do
+    #   include_buffer = (first-3)..(last+1)
+    #   with_buffer = String.slice(paragraph, include_buffer)
+    #   if Regex.match?(~r/\n\n[^\n]+\n\n/, with_buffer) do
+    #     {:ok, first..last}
+    #   else
+    #     perhaps_shift(paragraph, (first+1)..(last+1), remaining_shifts - 1)
+    #   end
+    # end
+
+    # def split_text(text, at_exactly: {first, last}) do
+    #   {prefix, remainder} = String.split_at(text, first-1)
+    #   {fragment, suffix} = String.split_at(remainder, last-first+1)
+    #   [prefix, fragment, suffix]
+    # end
   end
 end
--- prototypes/p002_watch_contours/test/paragraph_focus/motor/move_fragment_test.exs
+++ prototypes/p002_watch_contours/test/paragraph_focus/motor/move_fragment_test.exs
@@ -2,132 +2,185 @@ defmodule AppAnimal.ParagraphFocus.MoveFragmentTest do
   use ExUnit.Case
   alias AppAnimal.ParagraphFocus.Motor.MoveFragment, as: UT
   import FlowAssertions.TabularA
-  import FlowAssertions.MiscA
-
-
-  test "possibly shifted" do
-    returns = run_and_assert(&UT.accounting_for_edits/2)
-    #"123\n\n678\n\nbcd"
-    ["123\n\n678\n\nbcd", 6..8] |> returns.({:ok, 6..8})
-
-    #"123\n\n678\n\nbcd"
-    ["_123\n\n678\n\nbcd", 6..8] |> returns.({:ok, 7..9})  # We tolarate an added char in prefix
-    # ^
+  # import FlowAssertions.MiscA
+  
+  def para(text, cursor \\ 1), do: %{text: text, cursor: cursor}
+  
+  test "locating a moving fragment" do
+    template = "0123456\n\n9abcd\n\ngh\n"
+    original_range = 9..13
     
-    #"123\n\n678\n\nbcd"
-    ["_123_\n\n678\n\nbcd", 6..8] |> returns.({:ok, 8..10}) # ... and two.
-    #     ^
-    
-    #"123\n\n678\n\nbcd"
-    ["_12_3_\n\n678\n\nbcd", 6..8] |> returns.({:ok, 9..11}) # ... and three
-    #    ^
-
-    # But four is too much of a change: the app_animal should wait.
-    #"123\n\n678\n\nbcd"
-    ["__12_3_\n\n678\n\nbcd", 6..8] |> returns.(:error)
-
-    # Changes past the end are fine: they don't affect the snippet.
-    ["123\n\n678\n\nbcd________________", 6..8] |> returns.({:ok, 6..8})
-
-    # Note that destruction of one of the adjoining newlines is also an error.
-    ["123\n678\n\nbcd", 6..8] |> returns.(:error)
-    #    ^^
-    ["123\n\n678\nbcd", 6..8] |> returns.(:error)
-    #            ^^
-  end
-
-  test "cursor_not_in_way" do
-    reference = "0\n\n34\n\n7"  #
-    #             ^1 is safely distant because the character will appear before the \n
-    #               ^2 is too close: it will split the gap
-    #                     ^ 6 is too close: ditto
-    #                       ^7 is safely distant
-    #
-    is_judged = run_and_assert(fn cursor_at ->
-      paragraph = %{text: reference, cursor: cursor_at}
-      UT.cursor_relationship(paragraph, 3..4)
+    returns = run_and_assert(fn text ->
+      UT.grip_fragment(para(text), at_roughly: original_range)
     end)
+    
+    template |> returns.({:ok, 9..13})    # well, of course it finds it if it didn't move
 
-    is_judged.(1, :safely_distant)
-    2 |> is_judged.(:too_close)
-    6 |> is_judged.(:too_close)
-    7 |> is_judged.(:safely_distant)
+    # shift to the right
+    move_right = &(String.pad_leading(template, String.length(template) + &1, "_"))
+    move_right.(1) |> returns.({:ok, 10..14})
+
+    # shift to the left
+    move_left = &(String.split_at(template, &1)) |> elem(1)
+    IO.inspect("------")
+    IO.inspect(move_left.(1))
+    move_left.(1) |> returns.({:ok, 8..12})
+
   end
 
-  describe "grab fragment: changes in the paragraph" do
-    test "there's been no change in the paragraph" do 
-      %{text: "123\n\n678\n\nbcd", cursor: 1} 
-      |> UT.grab_fragment(at_roughly: 6..8)   # this is the range originally seen.
-      |> assert_equal({:ok, ["123\n", "\n678\n", "\nbcd"]})
-    end
+  # the search bounds are twice the original fragment (half the fragment on both sides)
+  # with enouggh room for two gap characters on either end.
+  test "the fragment is allowed to move but not so much" do 
+    returns = run_and_assert(&(UT.allowed_range(&1, 100)))
 
-    test "there's been an insertion before the range" do
-      %{text: "___123\n\n678\n\nbcd", cursor: 3}
-      |> UT.grab_fragment(at_roughly: 6..8)
-      |> assert_equal({:ok, ["___123\n", "\n678\n", "\nbcd"]})
-    end
+    6..7  |> returns.(3..10)
+    6..8  |> returns.(3..11)  # rounds toward zero
+    5..8  |> returns.(1..12)
 
-    test "too much change" do
-      %{text: "__________123\n\n678\n\nbcd", cursor: 10}
-      |> UT.grab_fragment(at_roughly: 6..8)
-      |> assert_equal(:error)
-    end
+    6..6  |> returns.(4..8)  # silly
+
+    # Don't exceed boundaries
+    96..99  |> returns.(92..100)
+    1..4  |> returns.(0..8)
   end
 
-  test "grab_fragment respond to cursor position, without text addition" do
-    text = "123\n\n678\n\nbcd"
-    successful_split = ["123\n", "\n678\n", "\nbcd"]
+  # test "detection of double quotes" do
+  #   returns = run_and_assert(fn text -> UT.new_end(text, 4..6) end)
+
+
+  #   "123456\n\n" |> returns.(6)
+  #   "123456\n\n" |> returns.(7)
+  #   "123456\n\n" |> returns.(8)
+
+  #   "123\n\n" |> returns.(:error)   # searches from the beginning.
+  #   "12\n\n"    |> returns.(:error)   # too short
+  #   "1\n\n"     |> returns.(:error)   # ditto
+
+  # end
+
+  # test "possibly shifted" do
+  #   returns = run_and_assert(&UT.accounting_for_edits/2)
+  #   #"123\n\n678\n\nbcd"
+  #   ["123\n\n678\n\nbcd", 6..8] |> returns.({:ok, 6..8})
+
+  #   #"123\n\n678\n\nbcd"
+  #   ["_123\n\n678\n\nbcd", 6..8] |> returns.({:ok, 7..9})  # We tolarate an added char in prefix
+  #   # ^
     
-    returns = run_and_assert(fn cursor_at ->
-      %{text: text, cursor: cursor_at}
-      |> UT.grab_fragment(at_roughly: 6..8)   # this is the range originally seen.
-    end)
-
-    # within that range
-    7 |> returns.(:error)
-
-    # boundary newlines
-    4  |> returns.({:ok, successful_split})
-    5  |> returns.(:error)
-    10 |> returns.(:error)
-    11 |> returns.({:ok, successful_split})
-  end
-
-
-  test "it is the *shifted* range that matters" do
-    # Just to explain what's going on:
-    # Suppose we start with this:
-    # "1234\n\n7\n\na"
-    # It would have text range 7..7, and it would reject a cursor within 6..9
+  #   #"123\n\n678\n\nbcd"
+  #   ["_123_\n\n678\n\nbcd", 6..8] |> returns.({:ok, 8..10}) # ... and two.
+  #   #     ^
     
-    # However, we make this shift by 3:
-    right3 = "___1234\n\n7\n\na"
-    # The forbidden zone is now 9-12
-    # A successful split is
-    right_success = ["___1234\n", "\n7\n", "\na"]
+  #   #"123\n\n678\n\nbcd"
+  #   ["_12_3_\n\n678\n\nbcd", 6..8] |> returns.({:ok, 9..11}) # ... and three
+  #   #    ^
+
+  #   # But four is too much of a change: the app_animal should wait.
+  #   #"123\n\n678\n\nbcd"
+  #   ["__12_3_\n\n678\n\nbcd", 6..8] |> returns.(:error)
+
+  #   # Changes past the end are fine: they don't affect the snippet.
+  #   ["123\n\n678\n\nbcd________________", 6..8] |> returns.({:ok, 6..8})
+
+  #   # Note that destruction of one of the adjoining newlines is also an error.
+  #   ["123\n678\n\nbcd", 6..8] |> returns.(:error)
+  #   #    ^^
+  #   ["123\n\n678\nbcd", 6..8] |> returns.(:error)
+  #   #            ^^
+  # end
+
+  # test "cursor_not_in_way" do
+  #   reference = "0\n\n34\n\n7"  #
+  #   #             ^1 is safely distant because the character will appear before the \n
+  #   #               ^2 is too close: it will split the gap
+  #   #                     ^ 6 is too close: ditto
+  #   #                       ^7 is safely distant
+  #   #
+  #   is_judged = run_and_assert(fn cursor_at ->
+  #     paragraph = %{text: reference, cursor: cursor_at}
+  #     UT.cursor_relationship(paragraph, 3..4)
+  #   end)
+
+  #   is_judged.(1, :safely_distant)
+  #   2 |> is_judged.(:too_close)
+  #   6 |> is_judged.(:too_close)
+  #   7 |> is_judged.(:safely_distant)
+  # end
+
+  # describe "grab fragment: changes in the paragraph" do
+  #   test "there's been no change in the paragraph" do 
+  #     %{text: "123\n\n678\n\nbcd", cursor: 1} 
+  #     |> UT.grab_fragment(at_roughly: 6..8)   # this is the range originally seen.
+  #     |> assert_equal({:ok, ["123\n", "\n678\n", "\nbcd"]})
+  #   end
+
+  #   test "there's been an insertion before the range" do
+  #     %{text: "___123\n\n678\n\nbcd", cursor: 3}
+  #     |> UT.grab_fragment(at_roughly: 6..8)
+  #     |> assert_equal({:ok, ["___123\n", "\n678\n", "\nbcd"]})
+  #   end
+
+  #   test "too much change" do
+  #     %{text: "__________123\n\n678\n\nbcd", cursor: 10}
+  #     |> UT.grab_fragment(at_roughly: 6..8)
+  #     |> assert_equal(:error)
+  #   end
+  # end
+
+  # test "grab_fragment respond to cursor position, without text addition" do
+  #   text = "123\n\n678\n\nbcd"
+  #   successful_split = ["123\n", "\n678\n", "\nbcd"]
     
-    returns = run_and_assert(fn text, cursor ->
-      %{text: text, cursor: cursor}
-      |> UT.grab_fragment(at_roughly: 7..7)
-    end)
+  #   returns = run_and_assert(fn cursor_at ->
+  #     %{text: text, cursor: cursor_at}
+  #     |> UT.grab_fragment(at_roughly: 6..8)   # this is the range originally seen.
+  #   end)
 
-    [right3, 8 ] |> returns.({:ok, right_success})
-    [right3, 9 ] |> returns.(:error)
-    [right3, 12] |> returns.(:error)
-    [right3, 13] |> returns.({:ok, right_success})
+  #   # within that range
+  #   7 |> returns.(:error)
+
+  #   # boundary newlines
+  #   4  |> returns.({:ok, successful_split})
+  #   5  |> returns.(:error)
+  #   10 |> returns.(:error)
+  #   11 |> returns.({:ok, successful_split})
+  # end
 
 
-    # Again, we choose this to start with:
-    # "1234\n\n7\n\na"
-    # However, we make this shift by deleting three characters:
-    left3 = "4\n\n7\n\na"
-    # The forbidden zone is now 1..5
-    # A successful split is
-    left_success = ["4\n", "\n7\n", "\na"]
-
-    IO.puts "HI"
-    [left3, 3] |> returns.({:ok, left_success})
+  # test "it is the *shifted* range that matters" do
+  #   # Just to explain what's going on:
+  #   # Suppose we start with this:
+  #   # "1234\n\n7\n\na"
+  #   # It would have text range 7..7, and it would reject a cursor within 6..9
     
-  end
+  #   # However, we make this shift by 3:
+  #   right3 = "___1234\n\n7\n\na"
+  #   # The forbidden zone is now 9-12
+  #   # A successful split is
+  #   right_success = ["___1234\n", "\n7\n", "\na"]
+    
+  #   returns = run_and_assert(fn text, cursor ->
+  #     %{text: text, cursor: cursor}
+  #     |> UT.grab_fragment(at_roughly: 7..7)
+  #   end)
+
+  #   [right3, 8 ] |> returns.({:ok, right_success})
+  #   [right3, 9 ] |> returns.(:error)
+  #   [right3, 12] |> returns.(:error)
+  #   [right3, 13] |> returns.({:ok, right_success})
+
+
+  #   # Again, we choose this to start with:
+  #   # "1234\n\n7\n\na"
+  #   # However, we make this shift by deleting three characters:
+  #   left3 = "4\n\n7\n\na"
+  #   # The forbidden zone is now 1..5
+  #   # A successful split is
+  #   left_success = ["4\n", "\n7\n", "\na"]
+
+  #   IO.puts "HI"
+  #   [left3, 3] |> returns.({:ok, left_success})
+    
+  # end
 end
 
