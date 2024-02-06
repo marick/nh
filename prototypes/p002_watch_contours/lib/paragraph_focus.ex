bef9152b9b32362f691a11672f04142177c414e9
Brian Marick <marick@exampler.com>
2024-02-06 13:39:07 -0600

    flymake indentation blues

--- prototypes/p002_watch_contours/lib/paragraph_focus.ex
+++ prototypes/p002_watch_contours/lib/paragraph_focus.ex
@@ -2,18 +2,17 @@ defmodule AppAnimal.ParagraphFocus do
   use GenServer
   require Logger
 
-   alias AppAnimal.ParagraphFocus.{Environment, Perceptual}
-   alias AppAnimal.Neural.Oscillator
-   alias Perceptual.EdgeDetection
-   require Logger
-
-   # Client
-
-   def start_link(paragraph_state) do
-     GenServer.start_link(__MODULE__, paragraph_state)
-   end
-
-
+  alias AppAnimal.ParagraphFocus.{Environment, Perceptual}
+  alias AppAnimal.Neural.Oscillator
+  alias Perceptual.EdgeDetection
+  require Logger
+  
+  # Client
+  
+  def start_link(paragraph_state) do
+    GenServer.start_link(__MODULE__, paragraph_state)
+  end
+  
   # Server
   
 
