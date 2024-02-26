79d9d1f3111ff1b16b11417ffd5286a1a20c2364
Brian Marick <marick@exampler.com>
2024-02-26 16:19:01 -0600

    Improve functions

--- prototypes/p003_test_first/lib/neural/cluster_makers.ex
+++ prototypes/p003_test_first/lib/neural/cluster_makers.ex
@@ -5,13 +5,9 @@ defmodule AppAnimal.Neural.ClusterMakers do
     module.new(name, handle_pulse)
   end
 
-  def circular_cluster(name, handle_pulse) when is_function(handle_pulse) do
-    circular_cluster(name,
-                     fn _configuration -> "process has no mutable state" end,
-                     handle_pulse)
-  end
-
-  def circular_cluster(name, initialize, handle_pulse) do
+  def circular_cluster(name, handle_pulse, keys \\ []) when is_function(handle_pulse) do
+    initialize = Keyword.get(keys, :initialize_mutable,
+                             fn _configuration -> "process has no mutable state" end)
     handlers = %{initialize: initialize, pulse: handle_pulse}
     %Neural.CircularCluster{name: name, handlers: handlers}
   end
--- prototypes/p003_test_first/test/neural/switchboard_test.exs
+++ prototypes/p003_test_first/test/neural/switchboard_test.exs
@@ -19,48 +19,50 @@ defmodule AppAnimal.Neural.SwitchboardTest do
     network = N.start(clusters)
     state = %UT{environment: "irrelevant", network: network}
     start_link_supervised!({UT, state})
-  end    
-    
-  test "a single-cluster chain" do
-    switchboard = switchboard_from([circular_cluster(:some_cluster, pulse_to_test())])
-    UT.initial_pulse(to: :some_cluster, carrying: "pulse data", via: switchboard)
-    assert_receive("pulse data")
   end
 
-  test "a transmission of pulses" do
-    handle_pulse = fn pulse_data, configuration, mutable ->
-      configuration.send_pulse_downstream.(carrying: pulse_data + 1)
-      mutable
+  describe "circular cluster handling" do 
+    test "a single-cluster chain" do
+      switchboard = switchboard_from([circular_cluster(:some_cluster, pulse_to_test())])
+      UT.initial_pulse(to: :some_cluster, carrying: "pulse data", via: switchboard)
+      assert_receive("pulse data")
     end
 
-    first = circular_cluster(:first, handle_pulse)
-    second = circular_cluster(:second, pulse_to_test())
-    switchboard = switchboard_from([first, second])
-    
-    UT.initial_pulse(to: :first, carrying: 1, via: switchboard)
-    assert_receive(2)
-  end
+    test "a transmission of pulses" do
+      handle_pulse = fn pulse_data, configuration, mutable ->
+        configuration.send_pulse_downstream.(carrying: pulse_data + 1)
+        mutable
+      end
 
-  test "succeeding pulses go to the same process" do
-    initializer = fn _configuration -> [] end
-    handle_pulse = fn :nothing, configuration, mutable ->
-      mutated = [self() | mutable]
-      configuration.send_pulse_downstream.(carrying: mutated)
-      mutated
+      first = circular_cluster(:first, handle_pulse)
+      second = circular_cluster(:second, pulse_to_test())
+      switchboard = switchboard_from([first, second])
+    
+      UT.initial_pulse(to: :first, carrying: 1, via: switchboard)
+      assert_receive(2)
     end
-    
-    first = circular_cluster(:first, initializer, handle_pulse)
-    second = circular_cluster(:second, pulse_to_test())
-    switchboard = switchboard_from([first, second])
-    
-    UT.initial_pulse(to: :first, carrying: :nothing, via: switchboard)
-    [first_pid] = assert_receive(_)
 
-    UT.initial_pulse(to: :first, carrying: :nothing, via: switchboard)
-    assert_receive([^first_pid, ^first_pid])
-  end
+    test "succeeding pulses go to the same process" do
+      initializer = fn _configuration -> [] end
+      handle_pulse = fn :nothing, configuration, mutable ->
+        mutated = [self() | mutable]
+        configuration.send_pulse_downstream.(carrying: mutated)
+        mutated
+      end
+    
+      first = circular_cluster(:first, handle_pulse, initialize_mutable: initializer)
+      second = circular_cluster(:second, pulse_to_test())
+      switchboard = switchboard_from([first, second])
+    
+      UT.initial_pulse(to: :first, carrying: :nothing, via: switchboard)
+      [first_pid] = assert_receive(_)
 
-  @tag :skip
-  test "... however, processes 'age out'" do
+      UT.initial_pulse(to: :first, carrying: :nothing, via: switchboard)
+      assert_receive([^first_pid, ^first_pid])
+    end
+
+    @tag :skip
+    test "... however, processes 'age out'" do
+    end
   end
 end
