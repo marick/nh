1640366d86b4b19761711f05440de6c6ee754ddd
Brian Marick <marick@exampler.com>
2024-03-22 15:20:36 -0500

    Some documentation.

--- prototypes/p003_test_first/CONVENTIONS.md
+++ prototypes/p003_test_first/CONVENTIONS.md
@@ -1,4 +1,4 @@
-### Suffixes for structs, pids, and lenses
+### Suffixes
 
 A particular struct, such as `CircularProcess`, may be referred to
 both as the struct itself and a pid that names a process that uses the
@@ -10,6 +10,10 @@ Similarly, there's sometimes a question of whether a name refers to
 the *field* of a structure or a *lens* that refers to that
 field. Therefore, lenses begin with `l_`.
 
+When it's not clear that a name is bound to a function, it begins with `f_`. A function-making function is prefixed with `mkfn_`. 
+
 Not super-happy about reinventing
 [Hungarian Notation](https://en.wikipedia.org/wiki/Hungarian_notation),
-but oh well.
+but oh well. 
+
+I will, as whim takes me, break the conventions or not convert old code.
--- prototypes/p003_test_first/lib/cluster/calc.ex
+++ prototypes/p003_test_first/lib/cluster/calc.ex
@@ -2,26 +2,71 @@ alias AppAnimal.Cluster
 alias Cluster.Calc
 
 defmodule Calc do
+  @moduledoc """
 
-  def run(calc, on: pulse_data, with_state: process_state) when is_function(calc, 1) do
+  Each cluster surrounds a function. It's convenient to describe linear and
+  circular clusters separately.
+
+  ### Linear clusters (tasks)
+
+  The function must take a single argument. The canonical two return values are
+
+  For linear clusters:
+  `{:no_pulse}`           - there is to be no outgoing pulse
+  `{:pulse, pulse_data}`  - the data is to be sent on.
+
+  As a convenience, these two kinds of result are translated into one of the above:
+
+  `:no_pulse`             - `{:no_pulse}`
+  any other result        - `{:pulse, pulse_data}`
+
+  ### Circular clusters
+
+  The function may take either one or two argumnets. If one, it's the
+  pulse data. If two, the second argument is the current value of the
+  mutable state (*not* an entire cluster structure).
+
+  For a single-argument function, the mutable state is left
+  unchanged. These are the two kinds of return value:
+
+  `:no_pulse`            - there is to be no outgoing pulse
+  any other result       - the data is to be sent on.
+
+  For a two-argument function, the two canonical return values are:
+
+  `{:pulse, pulse_data, next_state}`  - the data is to be sent on and the state updated.
+  `{:no_pulse,          next_state}`  - the cluster produces no pulse, but the state is updated.
+  
+  For convenience, the following two forms are also used:
+
+  `:no_pulse`            - there is no pulse and the state is left unchanged
+  any other value        - the value is sent in a pulse, but the state is left unchanged.        
+
+  ``
+  
+  
+
+  """
+
+  def run(calc, on: pulse_data, with_state: previously) when is_function(calc, 1) do
     case calc.(pulse_data) do 
       :no_pulse ->
-        {:no_pulse, process_state}
+        {:no_pulse, previously}
       result -> 
-        {:pulse, result, process_state}
+        {:pulse, result, previously}
     end
   end
 
-  def run(calc, on: pulse_data, with_state: process_state) when is_function(calc, 2) do
-    case calc.(pulse_data, process_state) do
-      {:pulse, _, _} = verbatim ->
+  def run(calc, on: pulse_data, with_state: previously) when is_function(calc, 2) do
+    case calc.(pulse_data, previously) do
+      {:pulse, _pulse_data, _next_previously} = verbatim ->
         verbatim
-      {:no_pulse, _} = verbatim ->
+      {:no_pulse, _next_previously} = verbatim ->
         verbatim
       :no_pulse ->
-        {:no_pulse, process_state}
+        {:no_pulse, previously}
       singleton_result ->
-        {:pulse, singleton_result, process_state}
+        {:pulse, singleton_result, previously}
     end
   end
 
@@ -34,14 +79,16 @@ defmodule Calc do
     end
   end
 
+  #
 
-  def maybe_pulse(tuple, pulser) when is_tuple(tuple) do
+  @doc "Use `f_send_pulse` to send pulse data iff the `tuple` argument so indicates."
+  def maybe_pulse(tuple, f_send_pulse) when is_tuple(tuple) do
     case elem(tuple, 0) do
       :no_pulse ->
         :do_nothing
       :pulse -> 
         pulse_data = elem(tuple, 1)
-        pulser.(pulse_data)
+        f_send_pulse.(pulse_data)
     end
     tuple
   end
