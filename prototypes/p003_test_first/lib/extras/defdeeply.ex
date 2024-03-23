aef8f88e90c2b36802ba66e9369efd72c644193c
Brian Marick <marick@exampler.com>
2024-03-23 10:45:47 -0500

    Macro foolishness.

---
+++ prototypes/p003_test_first/lib/extras/defdeeply.ex
@@ -0,0 +1,76 @@
+defmodule AppAnimal.Extras.DefDeeply do 
+  @moduledoc """
+  `defdeeply` expands into two functions. Consider this:
+
+        defdeeply deeply_put(s_struct, lens, value), do: Lens.put(lens, s_struct, value)
+
+  On the one hand, that simply creates the equivalent `def`:
+
+        def       deeply_put(s_struct, lens, value), do: Lens.put(lens, s_struct, value)
+
+  That allows lens use to follow the
+  structure-you're-operating-on-first Elixir convention. Compare:
+
+        Map.put(   struct, :key,   value)
+        deeply_put(struct, lens(), value))
+        Lens.put  (lens, struct, value)
+
+  The second thing `defdeeply` does is create a same-named
+  `deeply_...` function that uses the *name* of the lens function,
+  instead of the function itself. Like this:
+
+        struct = %MyStruct{...}
+        deeply_put(struct, :lens, value)
+
+  That is equivalent to this call:
+
+        deeply_put(struct, MyStruct.lens(), value)
+
+  Note: the lenses are defined with `def` instead of `deflens`. I don't think that
+  matters.
+  """
+
+  defmacro defdeeply(head, do: body) do
+    case length(elem(head, 2)) do
+      3 ->
+        defdeeply_3(head, body)
+      2 ->
+        defdeeply_2(head, body)
+    end
+  end
+
+  def defdeeply_3(head, body) do
+    {name, _meta, [struct_name, lens, arg3]} = head
+    new_arg2 = call_to_lookup_lens(struct_name, lens)
+    quote do
+      def unquote(head) when is_atom(unquote(lens)) do
+        unquote(name)(unquote(struct_name),
+                      unquote(new_arg2),
+                      unquote(arg3))
+      end
+      def unquote(head), do: unquote(body)
+    end 
+  end
+
+  def defdeeply_2(head, body) do
+    {name, _meta, [struct_name, lens]} = head
+    new_arg2 = call_to_lookup_lens(struct_name, lens)
+
+    quote do
+      def unquote(head) when is_atom(unquote(lens)) do
+        unquote(name)(unquote(struct_name),
+                      unquote(new_arg2))
+      end
+      def unquote(head), do: unquote(body)
+    end
+  end
+
+  def call_to_lookup_lens(struct_name, lens) do
+    quote do
+      unquote(__MODULE__).lookup_lens(unquote(struct_name),
+                                      unquote(lens))
+    end
+  end
+  
+  def lookup_lens(s_struct, lens_name), do: apply(s_struct.__struct__, lens_name, [])
+end
--- prototypes/p003_test_first/lib/extras/kernel.ex
+++ prototypes/p003_test_first/lib/extras/kernel.ex
@@ -1,37 +1,35 @@
 defmodule AppAnimal.Extras.Kernel do
   use Private
+  import AppAnimal.Extras.DefDeeply
   
   def constantly(value), do: fn _ -> value end
   
   def pi([{tag, value}]), do: IO.puts "#{tag}: #{inspect value}"
 
+  @doc """
+  In addition to reordering the arguments to follow the usual structure-first convention,
+  It creates a function that takes a symbol naming a function in the struct argument's
+  module. That is, given:
 
-  def deeply_put(s_struct, lens_name, value) when is_atom(lens_name),
-      do: deeply_put(s_struct, lookup_lens(s_struct, lens_name), value)
-  def deeply_put(s_struct, lens, value),
-      do: Lens.put(lens, s_struct, value)
+     struct = %MyStruct{...}
 
-  def deeply_get_only(s_struct, lens_name) when is_atom(lens_name),
-      do: deeply_get_only(s_struct, lookup_lens(s_struct, lens_name))
-  def deeply_get_only(s_struct, lens),
-      do: Lens.one!(lens, s_struct)
+  ... `deeply_put(struct, :lens, value)` is the same as:
 
-  def deeply_get_all(s_struct, lens_name) when is_atom(lens_name),
-      do: deeply_get_all(s_struct, lookup_lens(s_struct, lens_name))
-  def deeply_get_all(s_struct, lens),
-      do: Lens.to_list(lens, s_struct)
+     deeply_put(struct, MyStruct.lens(), value)
+  """
+  
+  defdeeply deeply_put(s_struct, lens, value),
+            do: Lens.put(lens, s_struct, value)
+  
+  defdeeply deeply_get_only(s_struct, lens),
+            do: Lens.one!(lens, s_struct)
 
-  def deeply_map(s_struct, lens_name, f) when is_atom(lens_name),
-      do: deeply_map(s_struct, lookup_lens(s_struct, lens_name), f)
-  def deeply_map(s_struct, lens, f),
-      do: Lens.map(lens, s_struct, f)
+  defdeeply deeply_get_all(s_struct, lens),
+            do: Lens.to_list(lens, s_struct)
+  
+  defdeeply deeply_map(s_struct, lens, f),
+            do: Lens.map(lens, s_struct, f)
 
-  def deeply_side_effect(s_struct, lens_name, f) when is_atom(lens_name),
-      do: deeply_side_effect(s_struct, lookup_lens(s_struct, lens_name), f)
-  def deeply_side_effect(s_struct, lens, f),
-      do: Lens.each(lens, s_struct, f)
-
-  private do
-    def lookup_lens(s_struct, lens_name), do: apply(s_struct.__struct__, lens_name, [])
-  end
+  defdeeply deeply_side_effect(s_struct, lens, f),
+            do: Lens.each(lens, s_struct, f)
 end
