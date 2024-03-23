a2909bd91d32123cf7b130ae40706700073a2351
Brian Marick <marick@exampler.com>
2024-03-23 16:36:34 -0500

    Split off network-creation tests to their own file.

---
+++ prototypes/p003_test_first/test/system/network_make_test.exs
@@ -0,0 +1,81 @@
+alias AppAnimal.System.Network
+
+defmodule Network.MakeTest do
+  use ClusterCase, async: true
+  # Although `trace` and similar functions are provided, module-prefix-free, from
+  # ClusterCase, I'll qualify them to make it clearer what's being tested.
+  alias Network.Make, as: UT
+
+
+  defp named(names) when is_list(names),
+       do: Enum.map(names, &named/1)
+  defp named(name),
+       do: circular(name)
+
+  defp downstreams(network) do
+    for one <- deeply_get_all(network, :l_clusters), into: %{} do
+      {one.name, one.downstream}
+    end
+  end
+  
+  defp assert_connections(network, field_descriptions) do
+    network
+    |> downstreams
+    |> assert_fields(field_descriptions)
+  end
+  
+  describe "building a network (basics)" do
+    test "singleton" do
+      first = linear(:first)
+      assert UT.trace([first]).clusters_by_name.first == first
+    end
+
+    test "multiple clusters in a row (a 'trace')" do
+      named([:first, :second, :third])
+      |> UT.trace
+      |> assert_connections(first: [:second], second: [:third])
+    end
+
+    test "can build from multiple disjoint traces" do
+         UT.trace(named([:a1, :a2]))
+      |> UT.trace(named([:b1, :b2]))
+      |> assert_connections(a1: [:a2], a2: [],
+                            b1: [:b2], b2: [])
+    end
+  end
+
+  
+
+  describe "handling of duplicates" do 
+    test "new value does not overwrite an existing one" do
+      first = linear(:first, & &1+1000)
+      new_first = linear(:first, &Function.identity/1)
+      network = UT.trace([first, new_first])
+      assert network.clusters_by_name.first.calc == first.calc
+
+      # Note that there's a loop because some version of first appears twice in the
+      # trace.
+      assert network.clusters_by_name.first.downstream == [:first]
+    end
+
+    test "adding duplicates doesn't overwrite the first cluster, but it makes traces loopy" do
+      [first, second, third] = named([:first, :second, :third])
+
+      [first, second, first, third, second]
+      |> UT.trace
+      |> assert_connections(first: in_any_order([:second, :third]),
+                            second: [:first],
+                            third: [:second])
+    end
+
+    test "can add a branch starting at an existing node" do
+      # This is, in effect adding a duplicate
+      UT.trace(named([      :first,              :second_a, :third]))
+      |> UT.extend(     at: :first, with: named([:second_b, :third]))
+      |> assert_connections(first: in_any_order([:second_a, :second_b]),
+                            second_a: [:third],
+                            second_b: [:third])
+    end
+  end
+  
+end
--- prototypes/p003_test_first/test/system/network_test.exs
+++ prototypes/p003_test_first/test/system/network_test.exs
@@ -8,18 +8,6 @@ defmodule AppAnimal.System.NetworkTest do
   defp named(name),
        do: circular(name)
 
-  defp downstreams(network) do
-    for one <- deeply_get_all(network, :l_clusters), into: %{} do
-      {one.name, one.downstream}
-    end
-  end
-  
-  defp assert_connections(network, field_descriptions) do
-    network
-    |> downstreams
-    |> assert_fields(field_descriptions)
-  end
-  
   describe "lenses" do
     test "l_cluster_named" do 
       network = UT.new(first: "a cluster")
@@ -32,58 +20,7 @@ defmodule AppAnimal.System.NetworkTest do
     end
   end
   
-  describe "building a network (basics)" do
-    test "singleton" do
-      first = linear(:first)
-      assert trace([first]).clusters_by_name.first == first
-    end
 
-    test "multiple clusters in a row (a 'trace')" do
-      named([:first, :second, :third])
-      |> trace
-      |> assert_connections(first: [:second], second: [:third])
-    end
-
-    test "can build from multiple disjoint traces" do
-         trace(named([:a1, :a2]))
-      |> trace(named([:b1, :b2]))
-      |> assert_connections(a1: [:a2], a2: [],
-                            b1: [:b2], b2: [])
-    end
-  end
-
-
-  describe "handling of duplicates" do 
-    test "new value does not overwrite an existing one" do
-      first = linear(:first, & &1+1000)
-      new_first = linear(:first, &Function.identity/1)
-      network = trace([first, new_first])
-      assert network.clusters_by_name.first.calc == first.calc
-
-      # Note that there's a loop because some version of first appears twice in the
-      # trace.
-      assert network.clusters_by_name.first.downstream == [:first]
-    end
-
-    test "adding duplicates doesn't overwrite the first cluster, but it makes traces loopy" do
-      [first, second, third] = named([:first, :second, :third])
-
-      [first, second, first, third, second]
-      |> trace
-      |> assert_connections(first: in_any_order([:second, :third]),
-                            second: [:first],
-                            third: [:second])
-    end
-
-    test "can add a branch starting at an existing node" do
-      # This is, in effect adding a duplicate
-      trace(named([      :first,              :second_a, :third]))
-      |> Make.extend(     at: :first, with: named([:second_b, :third]))
-      |> assert_connections(first: in_any_order([:second_a, :second_b]),
-                            second_a: [:third],
-                            second_b: [:third])
-    end
-  end
 
   describe "handling of throbbing clusters" do
     setup do
