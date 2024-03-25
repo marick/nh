1fea04f22dc9d0b82b29e3f4d23048cb09887363
Brian Marick <marick@exampler.com>
2024-03-25 15:25:54 -0500

    Pull paragraph gap structure over from p002

---
+++ prototypes/p003_test_first/lib/calc/paragraph_gaps.ex
@@ -0,0 +1,68 @@
+alias AppAnimal.Calc
+
+defmodule Calc.ParagraphGaps do
+  use AppAnimal
+  
+  def describe_summary(summary),
+      do: Logger.info("gap structure: #{gap_string summary}")        
+  
+  def summarize(text), do: gap_structure(text)
+  
+  def gap_structure(string) do
+    # Three passes over the string, whee!
+    parts = decompose(string)
+    labels = classify(parts)
+    ranges = ranges(parts)
+
+    List.zip([labels, ranges])
+  end
+
+  private do
+    @gap_definition ~r/\n\n+/
+
+    def decompose(string) do
+      string
+      |> String.split(@gap_definition, include_captures: true)
+      |> Enum.reject(&(&1 == ""))
+    end
+
+    def classify(decomposed_string) do
+      Enum.map(decomposed_string, fn snippet ->
+	if String.match?(snippet, @gap_definition) do
+	  :gap
+	else
+	  :text
+	end
+      end)
+    end
+
+    def ranges(decomposed_string) do
+      reducer = fn string, {next_start, ranges} ->
+	length = String.length(string)
+	next_next_start = next_start + length
+	range = next_start..(next_start+length-1)
+	{next_next_start, [range | ranges]}
+      end
+
+      decomposed_string
+      |> Enum.reduce({0, []}, reducer)
+      |> elem(1)
+      |> Enum.reverse
+    end
+  end
+
+
+  def gap_string(structure) do
+    classifier = fn
+      {:text, _} -> "\u25A0"
+      {:gap,  _} -> "_"
+    end
+    
+    structure
+    |> Enum.map(classifier)
+    |> Enum.join
+  end
+end
+
+
+
---
+++ prototypes/p003_test_first/test/calc/paragraph_gaps_test.exs
@@ -0,0 +1,79 @@
+alias AppAnimal.Calc
+
+defmodule Calc.ParagraphGapsTest do
+  use ExUnit.Case, async: true
+  alias Calc.ParagraphGaps, as: UT
+  import FlowAssertions.TabularA
+  import FlowAssertions.MiscA
+
+  describe "utilities" do
+    test "decomposition into text and gaps" do
+      becomes = run_and_assert(&UT.decompose/1)
+
+      # Two or more newlines is a gap
+      "abc"             |> becomes.(["abc"])
+      "abc\n    def"	|> becomes.(["abc\n    def"])
+      "abc\n\n  def"	|> becomes.(["abc", "\n\n", "  def"])
+      "abc\n\n\ndef"	|> becomes.(["abc", "\n\n\n", "def"])
+
+      # Newlines at start or end.
+      "abc\n\ndef\n"	|> becomes.(["abc", "\n\n", "def\n"])
+      "\nab"           |> becomes.(["\nab"])
+      "\n\nabc"         |> becomes.(["\n\n", "abc"])
+      "abc\n\n"         |> becomes.(["abc", "\n\n"])
+
+      # Nothing but gaps
+      "\n"              |> becomes.(["\n"])
+      "\n\n"            |> becomes.(["\n\n"])
+      "\n\n\n"          |> becomes.(["\n\n\n"])
+
+      # Nothing at all
+      ""                |> becomes.([])
+    end
+
+    test "classify" do
+      becomes = run_and_assert(&UT.classify/1)
+
+      ["\n\n"]                 |> becomes.([:gap])
+      ["abc", "\n\n\n", "def"] |> becomes.([:text, :gap, :text])
+
+      # Not fooled by stray newlines
+      ["\nabc", "a\nb", "def\n"] |> becomes.([:text, :text, :text])
+
+      # The everpresent boundary case
+      [] |> becomes.([])                         
+    end
+
+    test "ranges" do
+      input = "abc\n\n\ndef\n"
+      [text1, gap, text2] = [0..2, 3..5, 6..9] = UT.decompose(input) |> UT.ranges
+
+      # Confirm that the ranges cover the characters I expected
+      selects = run_and_assert(&(String.slice(input, &1)))
+      text1 |> selects.("abc")
+      gap   |> selects.("\n\n\n")
+      text2 |> selects.("def\n")
+    end
+  end
+
+
+  test "gap_structure" do
+    "part\n\nfragment\n\n\ngap\n"
+    |> UT.gap_structure
+    |> assert_equals([text: 0..3, gap: 4..5, text: 6..13, gap: 14..16, text: 17..20])
+  end
+
+  test "printing gap structure" do
+    returns = fn input, expected ->
+      actual = UT.gap_string(input)   # I should really modify flow-assertions to know arity
+      assert actual == expected
+    end
+
+    meh = "irrelevant"
+
+    [text: meh]                      |> returns.("\u25A0")
+    [text: meh, text: meh]           |> returns.("\u25A0\u25A0")
+    [text: meh, gap: meh, text: meh] |> returns.("\u25A0_\u25A0")
+  end
+end
+
