478d976e1daeee3605d8167448fa304ea62ed6d4
Brian Marick <marick@exampler.com>
2024-03-26 14:35:23 -0500

    Moving code into throb_logic

--- prototypes/p003_test_first/lib/cluster/throb_logic.ex
+++ prototypes/p003_test_first/lib/cluster/throb_logic.ex
@@ -10,5 +10,19 @@ defmodule ThrobLogic do
     field :starting_strength, integer
   end
 
-  def new(start_at), do: %__MODULE__{current_strength: start_at, starting_strength: start_at}
+  def new(start_at) do
+    %__MODULE__{current_strength: start_at, starting_strength: start_at}
+  end
+
+  def note_pulse(s_throb_logic, _calc_value) do
+    s_throb_logic
+  end
+
+  def throb(s_throb_logic) do
+    
+    mutated = Map.update!(s_throb_logic, :current_strength, & &1-1)
+    if mutated.current_strength <= 0,
+         do: {:stop, mutated},
+         else: {:continue, mutated}
+  end
 end
--- prototypes/p003_test_first/test/cluster/throb_logic_test.exs
+++ prototypes/p003_test_first/test/cluster/throb_logic_test.exs
@@ -2,9 +2,28 @@ alias AppAnimal.Cluster
 
 defmodule Cluster.ThrobLogicTest do
   use ExUnit.Case, async: true
-  # alias System.ThrobLogic, as: UT
-
-
+  use FlowAssertions
+  alias Cluster.ThrobLogic, as: UT
   
 
+  describe "simply running down" do
+    test "a throb runs it down a bit" do
+      logic = UT.new(2)
+      assert {:continue, new_logic} = UT.throb(logic)
+
+      assert_field(new_logic, current_strength: 1)
+    end
+
+    test "stop when zero is hit" do
+      logic = UT.new(1)
+      assert {:stop, new_logic} = UT.throb(logic)
+
+      assert_field(new_logic, current_strength: 0)
+    end
+    
+    test "a pulse does not make a difference" do
+      logic = UT.new(1)
+      assert logic == UT.note_pulse(logic, :irrelevant_pulse_value)
+    end
+  end
 end
