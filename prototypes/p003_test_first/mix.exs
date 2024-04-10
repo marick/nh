11f5a344ea0b1fc39ab961d9add3ab8e9928191e
Brian Marick <marick@exampler.com>
2024-04-10 11:25:24 -0500

    deps.get bimap

--- prototypes/p003_test_first/mix.exs
+++ prototypes/p003_test_first/mix.exs
@@ -36,7 +36,7 @@ defmodule AppAnimal.MixProject do
       {:typedstruct, "~> 0.5.2"},
       {:typed_struct_lens, "~> 0.1.1"},
       {:ex_doc, "~> 0.31", only: :dev, runtime: false},
-      
+      {:bimap, "~> 1.3"},      
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
