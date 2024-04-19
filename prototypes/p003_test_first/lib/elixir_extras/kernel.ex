86b1ac7a5f5d3ae42c18a334672ee1451cbee2b7
Brian Marick <marick@exampler.com>
2024-04-19 16:44:29 -0500

    The `precondition` macro

--- prototypes/p003_test_first/lib/elixir_extras/kernel.ex
+++ prototypes/p003_test_first/lib/elixir_extras/kernel.ex
@@ -6,4 +6,12 @@ defmodule AppAnimal.Extras.Kernel do
   @doc "Sometimes `pi(tag: value)` fits better than `IO.inspect` or `dbg`."
   def pi([{tag, value}]), do: IO.puts "#{tag}: #{inspect value}"
 
+  defmacro precondition(expression) do
+    quote do
+      unless unquote(expression) do
+        dbg {"Precondition failure", unquote(expression)}
+        raise("Precondition failure")
+      end
+    end
+  end
 end
