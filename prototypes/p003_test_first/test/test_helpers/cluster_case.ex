ae2963f5b3aafbf1987c91df2141e266355ed2e6
Brian Marick <marick@exampler.com>
2024-04-21 17:19:18 -0500

    Add forward_to_test to ClusterCase.
    (New-format Cluster)

--- prototypes/p003_test_first/test/test_helpers/cluster_case.ex
+++ prototypes/p003_test_first/test/test_helpers/cluster_case.ex
@@ -90,6 +90,22 @@ defmodule ClusterCase do
              calc: kludge_a_calc}
   end
 
+  def forward_to_test(name \\ :endpoint) do
+    alias AppAnimal.Building.Parts
+    p_test = self()
+
+    # Normally, a pulse is sent *after* calculation. Here, we have the
+    # cluster not calculate anything but just send to the test pid.
+    # That's because the `System.Router` only knows how to do GenServer-type
+    # casting.
+    kludge_a_calc = fn arg ->
+      send(p_test, [arg, from: name])
+      :no_result
+    end
+
+    Parts.linear(name, kludge_a_calc, label: :test_endpoint)
+  end
+
   @doc "Receive a pulse from a `to_test` node"
   defmacro assert_test_receives(value, opts \\ [from: :endpoint]) do
     quote do
