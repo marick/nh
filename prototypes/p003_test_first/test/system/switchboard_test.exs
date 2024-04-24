a1c4b0fd85aa30721cc36572e9efca49811e4087
Brian Marick <marick@exampler.com>
2024-04-24 14:25:57 -0500

    last switchboard test ported

--- prototypes/p003_test_first/test/system/switchboard_test.exs
+++ prototypes/p003_test_first/test/system/switchboard_test.exs
@@ -53,26 +53,26 @@ defmodule System.SwitchboardTest do
     assert_test_receives([^pid, ^pid, ^pid])
   end
 
-  @tag :test_uses_sleep
   test "what happens when a circular cluster 'ages out'" do
-#    first = C.circular(:first, fn _pulse -> self() end, max_age: 2) |> dbg
+    first = C.circular(:first, fn _pulse -> self() end,
+                       throb: Cluster.Throb.counting_down_from(2))
 
-#    a = animal([first, to_test()])
+    a = animal([first, forward_to_test()])
 
-    # send_test_pulse(a, to: :first, carrying: :irrelevant)
-    # first_pid = assert_test_receives(_)
+    send_test_pulse(a, to: :first, carrying: :irrelevant)
+    first_pid = assert_test_receives(_)
 
-    # throb_all_active(a)
-    # send_test_pulse(a, to: :first, carrying: :irrelevant)
-    # assert_test_receives(^first_pid)
+    throb_all_active(a)
+    send_test_pulse(a, to: :first, carrying: :irrelevant)
+    assert_test_receives(^first_pid)
 
-    # throb_all_active(a)
-    # # Need to make sure there's time to handle the "down" message, else the pulse will
-    # # be lost. The app_animal must tolerate dropped messages.
-    # Process.sleep(100)
-    # send_test_pulse(a, to: :first, carrying: :irrelevant)
+    throb_all_active(a)
+    # Need to make sure there's time to handle the "down" message, else the pulse will
+    # be lost. The app_animal must tolerate dropped messages.
+    Process.sleep(100)
+    send_test_pulse(a, to: :first, carrying: :irrelevant)
 
-    # another_pid = assert_test_receives(_)
-    # refute another_pid == first_pid
+    another_pid = assert_test_receives(_)
+    refute another_pid == first_pid
   end
 end
