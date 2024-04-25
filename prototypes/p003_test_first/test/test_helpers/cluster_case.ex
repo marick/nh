b23c7b08ca93f469a5d36f6e5af21def93573be4
Brian Marick <marick@exampler.com>
2024-04-25 17:40:49 -0500

    a little rearrangement

--- prototypes/p003_test_first/test/test_helpers/cluster_case.ex
+++ prototypes/p003_test_first/test/test_helpers/cluster_case.ex
@@ -3,16 +3,22 @@ defmodule ClusterCase do
     quote do
       use ExUnit.Case, unquote(opts)
       use AppAnimal
-      alias AppAnimal.{System,Network,Cluster}
+      use FlowAssertions
+
+      alias AppAnimal.{System,Network,Cluster,Extras}
       alias System.{Switchboard,AffordanceLand,ActivityLogger}
+
+      use Extras.TestAwareProcessStarter
+
       import ClusterCase.Helpers
       import AppAnimal.ActivityLogAssertions
-      use FlowAssertions
+
       import Cluster.Make
-      import Network.ClusterMap
+      import Network.ClusterMap                          ### delete
+
       alias AppAnimal.Duration
       alias AppAnimal.ClusterBuilders, as: C
-      alias AppAnimal.NetworkBuilder.Process, as: Add
+      alias AppAnimal.NetworkBuilder.Process, as: Add    ### delete
     end
   end
 end
--- prototypes/p003_test_first/test/test_helpers/scenario_case.ex
+++ prototypes/p003_test_first/test/test_helpers/scenario_case.ex
@@ -2,18 +2,20 @@ defmodule ScenarioCase do
 
   defmacro __using__(opts) do
     quote do
-      alias AppAnimal.{System,Network,Extras}
       use ExUnit.Case, unquote(opts)
       use AppAnimal
       use FlowAssertions
-      use Extras.TestAwareProcessStarter
+
+      alias AppAnimal.{System,Network,Extras}
       alias System.{Switchboard,AffordanceLand,ActivityLogger}
-      require ScenarioCase.Helpers
+
+      use Extras.TestAwareProcessStarter
+
       import ScenarioCase.Helpers
       import AppAnimal.ActivityLogAssertions
+
       alias AppAnimal.Duration
       alias AppAnimal.ClusterBuilders, as: C
-      alias AppAnimal.NetworkBuilder.Process, as: Add
     end
   end
 end
