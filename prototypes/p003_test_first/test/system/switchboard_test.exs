97b845585bdf9ddc710378f6974abafd0eda54b2
Brian Marick <marick@exampler.com>
2024-04-28 15:19:49 -0500

    conversion

--- prototypes/p003_test_first/test/system/switchboard_test.exs
+++ prototypes/p003_test_first/test/system/switchboard_test.exs
@@ -2,7 +2,7 @@ alias AppAnimal.System
 
 
 defmodule System.SwitchboardTest do
-  use ClusterCase, async: true
+  use Scenario.Case, async: true
 
   # At this point, the switchboard is tested indirectly, via tests of the features
   # that make use of the switchboard.
