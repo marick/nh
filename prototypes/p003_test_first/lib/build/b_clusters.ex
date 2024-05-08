5f36e262232f1018628ba5873d43d16022dd975f
Brian Marick <marick@exampler.com>
2024-05-08 17:22:54 -0500

    documentation

--- prototypes/p003_test_first/lib/build/b_clusters.ex
+++ prototypes/p003_test_first/lib/build/b_clusters.ex
@@ -1,4 +1,8 @@
 defmodule AppAnimal.ClusterBuilders do
+  @moduledoc """
+  All the predefined cluster types.
+  """
+
   use AppAnimal
   use KeyConceptAliases
   use MoveableAliases
@@ -75,19 +79,39 @@ defmodule AppAnimal.ClusterBuilders do
 
   section "specializations of linear clusters" do
 
+      @doc """
+      Handle a pulse that represents an affordance.
+
+      The affordance has the same name as the cluster. The pulse is
+      sent on as-is.
+      """
     def perception_edge(name) do
       linear(name, label: :perception_edge)
     end
 
+      @doc """
+      Send an action into Affordance Land.
+
+      Whatever pulse the cluster receives is wrapped in the action.
+      """
     def action_edge(name) do
       linear(name, & System.Action.new(name, &1),
              label: :action_edge)
     end
 
+      @doc """
+      Apply the given function to the received pulse.
+
+      This is really no different than a `linear` cluster, but the `:summarizer` label
+      might be useful as documentation of purpose.
+      """
     def summarizer(name, calc) do
       linear(name, calc, label: :summarizer)
     end
 
+      @doc """
+      Forward data on, but only if the `predicate` is truthy.
+      """
     def gate(name, predicate) do
       f = fn pulse_data ->
         if predicate.(pulse_data),
@@ -101,6 +125,22 @@ defmodule AppAnimal.ClusterBuilders do
 
   section "specializations of circular structures" do
 
+      @doc """
+      Forward on only the first of a sequence of identical pulses.
+
+      If enough time passes, the cluster will "age out". Thus it is
+      possible for the downstream to receive duplicates, but only if
+      they are separated by enough time (by default,
+      `Duration.frequent_glance`).
+
+      Each new pulses will extend the lifespan by one "throb
+      interval", so frequent enough pulses will keep the cluster alive
+      indefinitely.
+
+      ## Options
+      * `initial_value`: pretend that the cluster has already received a value.
+      * `throb`: A `Throb` structure to override default behavior.
+      """
     def forward_unique(name, opts \\ []) do
       alias Cluster.Throb
 
@@ -123,6 +163,16 @@ defmodule AppAnimal.ClusterBuilders do
       circular(name, calc, updated_opts)
     end
 
+      @doc """
+         Retain a value until pulses quiet down.
+
+         The cluster is kept alive by pulses. When it finally ages out,
+         it will send its initial value downstream.
+
+         Note that the `:data` in later pulses is ignored. It's
+         intended for use in cases where you want to forward on that
+         something happened only once it stops happening.
+      """
     def delay(name, duration) do
       throb = Throb.counting_up_to(duration,
                                    on_pulse: &Throb.pulse_zeroes_lifespan/2,
@@ -135,6 +185,27 @@ defmodule AppAnimal.ClusterBuilders do
       circular(name, f_stash_pulse_data, throb: throb, label: :delay)
     end
 
+      @doc """
+      Something notable has happened, and it's time to let in-progress work die out
+      and request a new affordance.
+
+      The cluster operates in two stages. When it starts, it sends a `Pulse.suppress`
+      to downstream clusters and waits for a time.
+
+      When the time has elapsed, an `Action` is sent into Affordance Land. This action
+      contains the data from the pulse that prompted this circular cluster to start work.
+
+      I haven't decided what should happen if a second pulse arrives before the movement
+      is completed.
+
+      ## Options
+
+      * `movement_time` (required): The time to delay. Think of this as the time it takes
+        for the eyeball to shift over to something new.
+
+      * `action_type` (required): The name of the action to be sent. Its `:data` will be
+        the data of whatever pulse started
+      """
     def focus_shift(name, opts) do
       [movement_time, action_to_take] =
         Opts.required!(opts, [:movement_time, :action_type])
