b96ba3ebd5cd5f50be8d290aa444b7645bbd5d75
Brian Marick <marick@exampler.com>
2024-05-15 18:08:36 -0500

    comments

--- prototypes/p003_test_first/lib/app_animal/pids.ex
+++ prototypes/p003_test_first/lib/app_animal/pids.ex
@@ -1,5 +1,12 @@
 defmodule AppAnimal.Pids do
-  @moduledoc "The collection of pids that make up the whole system."
+  @moduledoc """
+  The collection of pids that make up the whole system.
+
+  This includes direct access to processes inside the `Network`
+  structure, namely `p_circular_clusters` and `p_timer`. Mostly used
+  by tests. I suppose there should be more information hiding
+  somewhere around here.
+  """
   use TypedStruct
 
   typedstruct do
@@ -8,6 +15,7 @@ defmodule AppAnimal.Pids do
     field :p_switchboard,       pid
     field :p_affordland,        pid    # This name is perhaps unfortunate
     field :p_logger,            pid
+
     field :p_circular_clusters, pid
     field :p_timer,             pid
   end
